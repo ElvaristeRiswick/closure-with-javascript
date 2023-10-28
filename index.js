f208db46a89d51ee0db76fd99105a464264cb371
Riswick <riswickelvariste@gmail.com>
2023-10-28 00:16:23 -0400

    closure with javascript

---
+++ index.js
@@ -0,0 +1,23 @@
+/*function greeding(greet) {
+  return function (name) {
+    console.log(`${greet} ${name}`);
+  };
+}
+
+let greedingSon = greeding("hi");
+greedingSon ("djimelove");
+
+greeding();
+
+const countNumber = function () {
+  let count = 0;
+  return function () {
+    count++;
+    console.log(`${count} number`);
+  };
+};
+
+let counter = countNumber();
+counter();
+counter();
+counter();*/
